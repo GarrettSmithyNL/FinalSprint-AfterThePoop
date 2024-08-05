5aa06d25ea09e3a2a666ba02a24a5cb8aa29f5e2
RodneyCStead <RodCStead@gmail.com>
2024-08-04 21:36:16 -0230

    Added User object followed by Buyer, seller and Admin objects

---
+++ src/Seller.java
@@ -0,0 +1,35 @@
+// src/Seller.java
+import java.util.ArrayList;
+
+public class Seller extends User {
+    private ProductDAO productDAO;
+
+    public Seller() {
+        this.productDAO = new ProductDAO();
+    }
+
+    // Function to add new products
+    public void addProduct(Product product) {
+        productDAO.add(product);
+    }
+
+    // Function to update current products
+    public void updateProduct(Product product) {
+        productDAO.update(product);
+    }
+
+    // Function to delete their own products
+    public void deleteProduct(int productId) {
+        Product product = productDAO.getById(productId);
+        if (product != null && product.getSellerId() == this.getUserId()) {
+            productDAO.delete(productId);
+        } else {
+            throw new IllegalArgumentException("You can only delete your own products!.");
+        }
+    }
+
+    // Function to view a list of products
+    public ArrayList<Product> viewProducts() {
+        return productDAO.getAll();
+    }
+}
