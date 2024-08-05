185a79f6d2ec405b728937ecf83e988151dc1c48
RodneyCStead <RodCStead@gmail.com>
2024-08-05 18:28:45 -0230

    Added company object and CompanyServices

---
+++ src/Company.java
@@ -0,0 +1,39 @@
+public class Company {
+    private int companyId;
+    private int addressId;
+    private String companyName;
+    private String companyPhone;
+
+    // Getters and Setters
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyPhone() {
+        return companyPhone;
+    }
+
+    public void setCompanyPhone(String companyPhone) {
+        this.companyPhone = companyPhone;
+    }
+}
---
+++ src/CompanyServices.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class CompanyServices {
+    private CompanyDAO companyDAO;
+
+    public CompanyServices() {
+        this.companyDAO = new CompanyDAO();
+    }
+
+    public ArrayList<Company> getAllCompanies() {
+        return companyDAO.getAll();
+    }
+
+    public Company getCompanyById(int id) {
+        return companyDAO.getById(id);
+    }
+
+    public void addCompany(Company company) {
+        companyDAO.insert(company);
+    }
+
+    public void updateCompany(Company company) {
+        companyDAO.update(company);
+    }
+
+    public void deleteCompany(Company company) {
+        companyDAO.delete(company);
+    }
+}
